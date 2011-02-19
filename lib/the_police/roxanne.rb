562be8e98a84df36b05d045efd9c3e8c15a36c6d
Mathias Biilmann <info@mathias-biilmann.net>
2011-02-19 19:35:26 +0100

    string instead of symbols in insert

--- lib/the_police/roxanne.rb
+++ lib/the_police/roxanne.rb
@@ -34,17 +34,18 @@ module ThePolice
     def log(env, result)
       request = Rack::Request.new(env)
       collection.insert({
-        :level         => "error",
-        :type          => "request",
-        :time          => Time.now.utc,
-        :data          => {
-          :method        => request.request_method,
-          :url           => request.url,
-          :params        => request.params,
-          :response_body => result.last.size == 1 && result.last[0].kind_of?(String) ? result.last[0] : "IO Stream",          
+        "level"         => "error",
+        "type"          => "request",
+        "time"          => Time.now.utc,
+        "data"          => {
+          "method"        => request.request_method,
+          "url"           => request.url,
+          "params"        => request.params,
+          "response_body" => result.last.size == 1 && result.last[0].kind_of?(String) ? result.last[0] : "IO Stream",          
         }
-      }, {:safe => false})
-    rescue
+      }, {"safe" => false})
+    rescue StandardError => e
+      puts "Error #{e} - #{e.backtrace}"
       # Lets make sure logging an error doesn't break stuff!
     end
   end
